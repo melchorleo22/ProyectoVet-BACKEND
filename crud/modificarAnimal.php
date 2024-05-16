abc2bcb910b1c53b96040a786e8a2746b120583d
Lourdes Ibañez <ibanezlourdes62@gmail.com>
2024-05-15 21:10:04 -0300

    falta modificar la imagen

--- crud/modificarAnimal.php
+++ crud/modificarAnimal.php
@@ -1,44 +1,46 @@
 <?php
-// Verificar si se han enviado los datos del formulario
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Verificar si se han proporcionado todos los campos necesarios
-    if (isset($_POST['animalID']) && isset($_POST['animalName']) && isset($_POST['animalSpecies']) && isset($_POST['animalGender']) && isset($_POST['animalCastrated'])) {
-        // Conexión a la base de datos
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "veterinaria_db";
+// Conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "veterinaria_db";
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-        // Verificar la conexión
-        if ($conn->connect_error) {
-            die("Error de conexión: " . $conn->connect_error);
-        }
-
-        // Obtener los datos del formulario
-        $id_animal = $_POST['animalID'];
-        $nombre = $_POST['animalName'];
-        $especie = $_POST['animalSpecies'];
-        $sexo = $_POST['animalGender'];
-        $castrado = $_POST['animalCastrated'];
-
-        // Consulta SQL para actualizar los datos del animal
-        $sql = "UPDATE animal SET nombre_animal='$nombre', especie='$especie', sexo='$sexo', castrado='$castrado' WHERE id_animal = $id_animal";
-
-        // Ejecutar la consulta SQL
-        if ($conn->query($sql) === TRUE) {
-            echo "Animal modificado exitosamente.";
-        } else {
-            echo "Error al modificar el animal: " . $conn->error;
-        }
-
-        // Cerrar la conexión a la base de datos
-        $conn->close();
-    } else {
-        echo "Todos los campos son obligatorios.";
-    }
-} else {
-    echo "Acceso no autorizado.";
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
 }
+
+function modificarAnimal($id_animal) {
+    global $conn;
+
+    // Obtener los datos del formulario
+    $nombre = $_POST['animalName'];
+    $especie = $_POST['animalSpecies'];
+    $sexo = $_POST['animalGender'];
+    $castrado = $_POST['animalCastrated'];
+
+    // Obtener el nombre de la imagen si se ha subido?
+    $imagen = isset($_FILES['animalImage']['name']) ? $_FILES['animalImage']['name'] : '';
+
+    // Consulta SQL para modificar el animal
+    $update = "UPDATE animal SET nombre_animal='$nombre', especie='$especie', sexo='$sexo', castrado='$castrado', imagen='$imagen' WHERE id_animal = $id_animal";
+    
+    $resultado = $conn->query($update);
+    if ($resultado === TRUE) {
+        echo "Animal modificado exitosamente.";
+    } else {
+        echo "Error al modificar el animal: " . $conn->error;
+    }
+}
+
+// Obtener el ID del animal a modificar desde el formulario
+$id_animal = $_POST['animalID'];
+
+// Le paso el id obtenido del formulario
+modificarAnimal($id_animal);
+
+// Cerrar la conexión a la base de datos
+$conn->close();
 ?>
